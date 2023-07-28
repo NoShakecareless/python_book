97ee10bf7e5359683ea71045a5baf65873f9fad2
python book <1848142281@qq.com>
2023-07-28 21:01:38 +0800

    part 1

---
+++ alien_invasion.py
@@ -0,0 +1,41 @@
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+
+
+def run_game():
+
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # 创建飞船
+    ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    bg_color = (230, 230, 230)
+    # 设置背景色
+    ship = Ship(ai_settings, screen)
+
+    # 游戏开始的主循环
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_aliens(aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+
+run_game()
