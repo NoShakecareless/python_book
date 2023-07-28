34c05af103c1dc4540c9971792763b5d5596d0f7
python book <1848142281@qq.com>
2023-07-28 21:09:05 +0800

    part 1

--- alien.py
+++ alien.py
@@ -28,5 +28,14 @@ class Alien(Sprite):
 
     def update(self):
         """向右移动外星人"""
-        self.x += self.ai_settings.alien_speed_factor
+        self.x += (self.ai_settings.alien_speed_factor *
+                   self.ai_settings.fleet_direction)
         self.rect.x = self.x
+
+    def check_edges(self):
+        """如果外星人位于屏幕边缘，就返回true"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
